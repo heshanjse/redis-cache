ba211fb75a6720fb805b547b693718f60a00e57c
Heshan Jayasinghe <heshanjayasinghe@Heshans-MacBook-Pro-2.local>
2020-03-19 12:18:55 +0800

    git commit -m "add node end point"

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fetch = require('node-fetch');
+//const radis = require('radis')
+
+const PORT = process.env.PORT || 5000;
+//const RADIS_PORT = process.env.PORT || 6379;
+
+//const client = radis.createClient(RADIS_PORT);
+const app = express();
+
+async function getRepos(req,res,next) {
+  try {
+    console.log('Fetching Data..');
+    const {username} = req.params;
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const data = await response.json();
+    res.send(data);
+
+  } catch (error) {
+    console.log(error)
+  }
+  
+}
+
+app.get('/repos/:username',getRepos);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+});
