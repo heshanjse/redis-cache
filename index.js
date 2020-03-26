db6596c93c1fe5e6b4ab90ad0b247a6276888d2c
Heshan Jayasinghe <heshanjayasinghe@Heshans-MacBook-Pro-2.local>
2020-03-26 12:42:06 +0800

    add new methods and radis docker

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const fetch = require('node-fetch');
-//const radis = require('radis')
+const redis = require('redis')
 
 const PORT = process.env.PORT || 5000;
-//const RADIS_PORT = process.env.PORT || 6379;
+const REDIS_PORT = process.env.PORT || 6379;
 
-//const client = radis.createClient(RADIS_PORT);
+const client = redis.createClient(REDIS_PORT);
 const app = express();
 //route(app);
+
+function setResponse(username,repos) {
+  return `${username} has ${repos} Github repos`
+}
 
 async function getRepos(req,res,next) {
   try {
@@ -15,15 +19,38 @@ async function getRepos(req,res,next) {
     const {username} = req.params;
     const response = await fetch(`https://api.github.com/users/${username}`);
     const data = await response.json();
-    res.send(data);
+    const repos = data.public_repos;
+
+    //set data to redis
+    client.setex(username,3600,repos);
+
+
+    res.send(setResponse(username,repos));
 
   } catch (error) {
     console.log(error)
   }
   
+  
 }
 
-app.get('/repos/:username',getRepos);
+//cache middleware
+function cache(req,res,next) {
+  const {username} = req.params;
+  client.get(username,(err,data) => {
+    if (err) throw err;
+
+    if (data !== null) {
+      res.send(setResponse(username,data));
+    }else{
+      next();
+    }
+
+  })
+  
+}
+
+app.get('/repos/:username',cache, getRepos);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
